ce352660070431049397a5709c18142a5fcbf9de
Chris Hunt <cdhunt@gmail.com>
2015-10-28 11:35:36 -0400

    Initial Commit

---
+++ Parse-Curl.Tests.ps1
@@ -0,0 +1,94 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+Describe "Curl Parsing" {
+    Context "Get URI" {
+        $params = 'curl http://www.telize.com/geoip' | Parse-Curl
+
+        It "Uri should be 'http://www.telize.com/geoip'" {
+            $params["Uri"] | Should Be 'http://www.telize.com/geoip'
+        }
+    }
+
+    Context "User agent" {
+        $params = 'curl --user-agent "Mozilla/4.0" http://www.telize.com/geoip' | Parse-Curl
+
+        It "Uri should be 'http://www.telize.com/geoip'" {
+            $params["Uri"] | Should Be 'http://www.telize.com/geoip'
+        }
+        It "UserAgent should be 'Mozilla/4.0'" {
+            $params["UserAgent"] | Should Be 'Mozilla/4.0'
+        }
+    }
+
+    Context "Headers" {
+        $params = 'curl -H "X-First-Name: Joe" http://www.telize.com/geoip' | Parse-Curl
+
+        It "Uri should be 'http://www.telize.com/geoip'" {
+            $params["Uri"] | Should Be 'http://www.telize.com/geoip'
+        }
+        It "Headers should be @{X-First-Name='Joe'}" {
+            $params["Headers"]["X-First-Name"] | Should Be 'Joe'
+        }
+    }
+
+    Context "Multiple headers" {
+        $params = 'curl -H "X-First-Name: Joe" --header "header:value" http://www.telize.com/geoip' | Parse-Curl
+
+        It "Uri should be 'http://www.telize.com/geoip'" {
+            $params["Uri"] | Should Be 'http://www.telize.com/geoip'
+        }
+        It "Headers should be @{X-First-Name='Joe'}" {
+            $params["Headers"]["X-First-Name"] | Should Be 'Joe'
+        }
+        It "Headers should be @{header='value'}" {
+            $params["Headers"]["header"] | Should Be 'value'
+        }
+    }
+
+    Context "With -d" {
+        $params = 'curl -d 1 http://www.telize.com/geoip' | Parse-Curl
+
+        It "Uri should be 'http://www.telize.com/geoip'" {
+            $params["Uri"] | Should Be 'http://www.telize.com/geoip'
+        }
+        It "Body should be '1'" {
+            $params["Body"] | Should Be "1"
+        }
+        It "Method should be 'Post'" {
+            $params["Method"] | Should Be "Post"
+        }
+    }
+
+    Context "With --data" {
+        $params = 'curl --data 1 http://www.telize.com/geoip' | Parse-Curl
+
+        It "Uri should be 'http://www.telize.com/geoip'" {
+            $params["Uri"] | Should Be 'http://www.telize.com/geoip'
+        }
+        It "Body should be '1'" {
+            $params["Body"] | Should Be "1"
+        }
+        It "Method should be 'Post'" {
+            $params["Method"] | Should Be "Post"
+        }
+    }
+
+    Context "With --data 1 --data text" {
+        $params = 'curl --data 1 --data text http://www.telize.com/geoip' | Parse-Curl
+
+        It "Should be 'http://www.telize.com/geoip'" {
+            $params["Uri"] | Should Be 'http://www.telize.com/geoip'
+        }
+        It "Body[0] should be '1'" {
+            $params["Body"][0] | Should Be "1"
+        }
+        It "Body[1] should be 'text'" {
+            $params["Body"][1] | Should Be "text"
+        }
+        It "Method should be 'Post'" {
+            $params["Method"] | Should Be "Post"
+        }
+    }
+}
---
+++ Parse-Curl.ps1
@@ -0,0 +1,93 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Parse-Curl
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([Hashtable])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
+        [String]
+        $InputObject
+    )
+
+    $ParamList = @{}
+
+    $objectArray = $InputObject | ConvertFrom-String
+
+    $index = 1
+
+    while ($index -lt ($objectArray | Get-Member -MemberType NoteProperty).Count) 
+    {
+
+        $term = $objectArray."P$index"
+
+        switch ($term)
+        {
+            'curl' {}
+            {$_ -eq '-D' -or $_ -eq '--data'} {
+                $index++
+                $ParamList["Body"] = Update-Body $ParamList["Body"] $objectArray."P$index"
+                if (!$ParamList["Method"]) { $ParamList["Method"] = "Post"}
+            }
+            {$_ -eq '-H' -or $_ -eq '--header'} {
+                $index++
+                $value = $objectArray."P$index".Trim('"')
+                if ($value[-1] -eq ':')
+                {
+                    $index++
+                    $value += $objectArray."P$index".Trim('"')
+                }
+                $ParamList["Headers"] = Update-Headers $ParamList["Headers"] $value
+            }
+            {$_ -eq '-A' -or $_ -eq '--user-agent'} {
+                $index++
+                if (!$ParamList["UserAgent"]) { $ParamList["UserAgent"] = $objectArray."P$index".Trim('"')}
+            }
+        }
+        $index++
+        
+    }
+    
+    $ParamList["Uri"] = $objectArray."P$index"
+
+    Write-Output $ParamList
+}
+
+function Update-Body ($body, [string]$data)
+{
+    if (!$body)
+    {
+        $body = @()
+    }
+
+    $body = @($body) + [System.Web.HttpUtility]::UrlEncode($data)
+
+    return $body
+}
+
+function Update-Headers ($headers, [string]$data)
+{
+    if (!$headers)
+    {
+        $headers = @{}
+    }
+
+    $dataArray = $data.Split(':')
+
+    $headers.Add($dataArray[0].Trim(), $dataArray[1].Trim())
+
+    return $headers
+}
