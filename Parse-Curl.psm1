9a80d9deb17b736bfe462ef894f06a93b71a79f8
Chris Hunt <cdhunt@gmail.com>
2015-11-20 13:38:14 -0500

    Added Proxy Function for Invoke-RestMethod that accepts a CurlString parameter

--- Parse-Curl.psm1
+++ Parse-Curl.psm1
@@ -89,3 +89,143 @@ function Update-Header ($headers, [string]$data)
 
     return $headers
 }
+
+Function Invoke-RestMethod
+{
+    [CmdletBinding(HelpUri='http://go.microsoft.com/fwlink/?LinkID=217034')]
+    param(
+        [Microsoft.PowerShell.Commands.WebRequestMethod]
+        ${Method},
+    
+        [switch]
+        ${UseBasicParsing},
+    
+        [Parameter(Mandatory=$false, Position=0)]
+        [ValidateNotNullOrEmpty()]
+        [uri]
+        ${Uri},
+    
+        [Microsoft.PowerShell.Commands.WebRequestSession]
+        ${WebSession},
+    
+        [Alias('SV')]
+        [string]
+        ${SessionVariable},
+    
+        [pscredential]
+        [System.Management.Automation.CredentialAttribute()]
+        ${Credential},
+    
+        [switch]
+        ${UseDefaultCredentials},
+    
+        [ValidateNotNullOrEmpty()]
+        [string]
+        ${CertificateThumbprint},
+    
+        [ValidateNotNull()]
+        [System.Security.Cryptography.X509Certificates.X509Certificate]
+        ${Certificate},
+    
+        [string]
+        ${UserAgent},
+    
+        [switch]
+        ${DisableKeepAlive},
+    
+        [int]
+        ${TimeoutSec},
+    
+        [System.Collections.IDictionary]
+        ${Headers},
+    
+        [ValidateRange(0, 2147483647)]
+        [int]
+        ${MaximumRedirection},
+    
+        [uri]
+        ${Proxy},
+    
+        [pscredential]
+        [System.Management.Automation.CredentialAttribute()]
+        ${ProxyCredential},
+    
+        [switch]
+        ${ProxyUseDefaultCredentials},
+    
+        [Parameter(ValueFromPipeline=$true)]
+        [System.Object]
+        ${Body},
+    
+        [string]
+        ${ContentType},
+    
+        [ValidateSet('chunked','compress','deflate','gzip','identity')]
+        [string]
+        ${TransferEncoding},
+    
+        [string]
+        ${InFile},
+    
+        [string]
+        ${OutFile},
+    
+        [switch]
+        ${PassThru},
+        
+        [ValidateNotNullOrEmpty()] 
+        [System.String] 
+        ${CurlString}
+    )
+    begin
+    {
+        try {
+            $outBuffer = $null
+            if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer))
+            {
+                $PSBoundParameters['OutBuffer'] = 1
+            }
+            $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand('Microsoft.PowerShell.Utility\Invoke-RestMethod', [System.Management.Automation.CommandTypes]::Cmdlet)
+            
+            if ($PSBoundParameters['CurlString'])
+            {
+                $parameters = $CurlString | ConvertFrom-CurlString
+                $scriptCmd = {& $wrappedCmd @parameters }
+            }
+            else
+            {
+                $scriptCmd = {& $wrappedCmd @PSBoundParameters }
+            }
+            
+            $steppablePipeline = $scriptCmd.GetSteppablePipeline($myInvocation.CommandOrigin)
+            $steppablePipeline.Begin($PSCmdlet)
+        } catch {
+            throw
+        }
+    }
+    
+    process
+    {
+        try {
+            $steppablePipeline.Process($_)
+        } catch {
+            throw
+        }
+    }
+    
+    end
+    {
+        try {
+            $steppablePipeline.End()
+        } catch {
+            throw
+        }
+    }
+    <#
+    
+    .ForwardHelpTargetName Microsoft.PowerShell.Utility\Invoke-RestMethod
+    .ForwardHelpCategory Cmdlet
+    
+    #>
+
+}
